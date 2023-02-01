7b0e8bfc1b2063d4d43cf96dd924d1ffa507a7cf
Wojciech Siewierski <wojciech@siewierski.eu>
2023-02-01 15:44:25 +0100

    Revert "Remove a redundant Package-Requires declaration from a side file"
    This reverts commit 7837cf9aaebc3f83dc357ecfcfd4f156a84d0d60.
    
    Restore the Package-Requires declaration for the sake of silencing the
    overzealous warnings.

--- tmsu-dired.el
+++ tmsu-dired.el
@@ -6,6 +6,7 @@
 ;; URL: https://github.com/vifon/tmsu.el
 ;; Keywords: files
 ;; Version: 0.9
+;; Package-Requires: ((emacs "28.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
