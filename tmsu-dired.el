7837cf9aaebc3f83dc357ecfcfd4f156a84d0d60
Wojciech Siewierski <wojciech@siewierski.eu>
2023-02-01 15:35:17 +0100

    Remove a redundant Package-Requires declaration from a side file

--- tmsu-dired.el
+++ tmsu-dired.el
@@ -6,7 +6,6 @@
 ;; URL: https://github.com/vifon/tmsu.el
 ;; Keywords: files
 ;; Version: 0.9
-;; Package-Requires: ((emacs "28.1"))
 
 ;; This program is free software; you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
