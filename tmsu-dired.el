8207b4140fd1b3eff2ec4b9818e67148139cb72f
Wojciech Siewierski <wojciech@siewierski.eu>
2023-08-08 00:18:42 +0200

    Remove the final slash from the dired top line for consistency
    This how is the original dired shows it, though not find-dired.
    I'd rather consider it a very minor purely visual bug in find-dired.

--- tmsu-dired.el
+++ tmsu-dired.el
@@ -278,7 +278,7 @@ Interactively ask for the FLAGS only if \\[universal-argument] got passed."
     (setq buffer-read-only nil)
     ;; Subdir headlerline must come first because the first marker in
     ;; subdir-alist points there.
-    (insert "  " dir ":\n")
+    (insert "  " (directory-file-name dir) ":\n")
     (when (bound-and-true-p dired-make-directory-clickable)
       (dired--make-directory-clickable))
     ;; Make second line a ``tmsu'' line in analogy to the ``total'' or
