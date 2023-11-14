8ff2f131667e69e9e8be63183e03eccd03c17694
Wojciech Siewierski <wojciech@siewierski.eu>
2023-11-14 03:12:01 +0100

    Add work-in-progress tests on my local database
    Eventually a test database should be created and added to the
    repository.  For a time being this is still far better than nothing at
    all, even if I'm the only one able to run it.

--- tmsu-tests.el
+++ tmsu-tests.el
@@ -7,3 +7,62 @@
   (should (equal (tmsu--escape-value "foo\\ bar") "foo\\\\\\ bar"))
   (should (equal (tmsu--escape-value "foo <bar>") "foo\\ \\<bar\\>"))
   (should (equal (tmsu--escape-value "foo (bar)") "foo\\ \\(bar\\)")))
+
+(ert-deftest tmsu-test-completion-assignment ()
+  (skip-unless (getenv "TMSU_TEST_MEDIA_DIR"))
+  (let* ((default-directory (getenv "TMSU_TEST_MEDIA_DIR"))
+         (completion-table (tmsu--completion tmsu--key-value-regex)))
+    (should (equal
+             (try-completion "gen" completion-table)
+             "genre"))
+    (should-not (try-completion "not gen" completion-table))
+    (should (equal
+             (try-completion "genre=com" completion-table)
+             "genre=comedy"))
+
+    (should (try-completion "year=19" completion-table))
+    (should (member
+             "year=1979"
+             (all-completions "year=19" completion-table)))
+    (should-not (member
+                 "year=2003"
+                 (all-completions "year=19" completion-table)))
+
+    (should-not (try-completion "year<19" completion-table))
+    (should-not (try-completion "year>19" completion-table))
+    (should-not (try-completion "year < 19" completion-table))
+    (should-not (try-completion "year > 19" completion-table))
+    (should-not (try-completion "year <= 19" completion-table))
+    (should-not (try-completion "year >= 19" completion-table))))
+
+(ert-deftest tmsu-test-completion-query ()
+  (skip-unless (getenv "TMSU_TEST_MEDIA_DIR"))
+  (let* ((default-directory (getenv "TMSU_TEST_MEDIA_DIR"))
+         (completion-table (tmsu--completion tmsu--expression-regex)))
+    (should (equal
+             (try-completion "gen" completion-table)
+             "genre"))
+    (should (equal
+             (try-completion "not gen" completion-table)
+             "not genre"))
+    (should (equal
+             (try-completion "genre=com" completion-table)
+             "genre=comedy"))
+    (should (equal
+             (try-completion "not genre=com" completion-table)
+             "not genre=comedy"))
+
+    (should (try-completion "year=19" completion-table))
+    (should (member
+             "year=1979"
+             (all-completions "year=19" completion-table)))
+    (should-not (member
+                 "year=2003"
+                 (all-completions "year=19" completion-table)))
+
+    (should (try-completion "year<19" completion-table))
+    (should (try-completion "year>19" completion-table))
+    (should (try-completion "year < 19" completion-table))
+    (should (try-completion "year > 19" completion-table))
+    (should (try-completion "year <= 19" completion-table))
+    (should (try-completion "year >= 19" completion-table))))
