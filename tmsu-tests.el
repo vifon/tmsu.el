c44e9042cd6894314a69652f3efba446550516b4
Wojciech Siewierski <wojciech@siewierski.eu>
2023-11-14 13:31:41 +0100

    Tests: Add membership checks to the test helper function

--- tmsu-tests.el
+++ tmsu-tests.el
@@ -17,6 +17,14 @@
         (should (try-completion input completion-table)))
        (`(,input nil)
         (should-not (try-completion input completion-table)))
+       (`(,input (member ,expected))
+        (should (member
+                 expected
+                 (all-completions input completion-table))))
+       (`(,input (member-not ,expected))
+        (should-not (member
+                     expected
+                     (all-completions input completion-table))))
        (`(,input ,expected)
         (should (equal (try-completion input completion-table)
                        expected)))))
@@ -34,20 +42,15 @@
        ("not gen"        nil)
        ("genre=com"      "genre=comedy")
        ("year=19"        t)
+       ("year=19"        (member     "year=1979"))
+       ("year=19"        (member-not "year=2003"))
        ("year<19"        nil)
        ("year>19"        nil)
        ("year < 19"      nil)
        ("year > 19"      nil)
        ("year <= 19"     nil)
        ("year >= 19"     nil)
-       ("staff=Makoto"   "staff=Makoto Shinkai")))
-
-    (should (member
-             "year=1979"
-             (all-completions "year=19" completion-table)))
-    (should-not (member
-                 "year=2003"
-                 (all-completions "year=19" completion-table)))))
+       ("staff=Makoto"   "staff=Makoto Shinkai")))))
 
 (ert-deftest tmsu-test-completion-query ()
   (skip-unless (getenv "TMSU_TEST_MEDIA_DIR"))
@@ -62,17 +65,12 @@
        ("genre=com"      "genre=comedy")
        ("not genre=com"  "not genre=comedy")
        ("year=19"        t)
+       ("year=19"        (member     "year=1979"))
+       ("year=19"        (member-not "year=2003"))
        ("year<19"        t)
        ("year>19"        t)
        ("year < 19"      t)
        ("year > 19"      t)
        ("year <= 19"     t)
        ("year >= 19"     t)
-       ("staff=Makoto"   "staff=Makoto\\ Shinkai")))
-
-    (should (member
-             "year=1979"
-             (all-completions "year=19" completion-table)))
-    (should-not (member
-                 "year=2003"
-                 (all-completions "year=19" completion-table)))))
+       ("staff=Makoto"   "staff=Makoto\\ Shinkai")))))
